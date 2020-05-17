cd086854e3f00ba913e0c12f234d8176ec047fcf
Saleh Sayeem <16103179@iubat.edu>
2020-05-17 23:02:19 +0600

    Initial

---
+++ RemoteValidation/Models/Customer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RemoteValidation.Models
+{
+    public class Customer
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "UserName is required")]
+        [Remote("IsUserNameExist","Customers",ErrorMessage = "User Name already available")]
+        public string UserName { get; set; }
+        [Required(ErrorMessage = "Email is required")]
+        [DataType(DataType.EmailAddress)]
+        [Remote("IsEmailExist", "Customers", ErrorMessage = "Email already available")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Password is required")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
